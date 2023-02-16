95c1bd1c7fa502ed715418377470c737d71fab66
Art Mills <art@hamcois.com>
2023-02-16 12:09:47 -0600

    Messing around with fundamentals.   Some of this is quite fun.

--- struct.go
+++ struct.go
@@ -8,12 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Please select an option")
-	fmt.Println("1) Print menu")
-	in := bufio.NewReader(os.Stdin)
-	choice, _ := in.ReadString('\n')
-	choice = strings.TrimSpace(choice)
-
 	type menuItem struct {
 		name   string
 		prices map[string]float64
@@ -24,6 +18,25 @@ func main() {
 		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
 	}
 
-	fmt.Println(menu)
+	fmt.Println("Please select an option")
+	fmt.Println("1) Print menu")
+	fmt.Println("2) Add item")
+	fmt.Println("3) quit")
+	in := bufio.NewReader(os.Stdin)
+	choice, _ := in.ReadString('\n')
+	//choice = strings.TrimSpace(choice)
 
+	switch strings.TrimSpace(choice) {
+	case "1":
+
+		for _, item := range menu {
+			fmt.Println(item.name)
+			fmt.Println(strings.Repeat("-", 10))
+			for size, price := range item.prices {
+				fmt.Printf("\t%10s%10.2f\n", size, price)
+			}
+		}
+	case "2":
+		fmt.Println("Please enter the name of the new item")
+	}
 }
