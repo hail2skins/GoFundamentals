8ce10e801365c7118cd39ec2c0bda99ef2b4bb27
Art Mills <art@hamcois.com>
2023-02-16 17:40:47 -0600

    Struct is the main main here :)

--- struct.go
+++ struct.go
@@ -1,42 +1,36 @@
 package main
 
 import (
+	"GoFundamentals/menu"
 	"bufio"
 	"fmt"
 	"os"
 	"strings"
+
+	_ "GoFundamentals/menu"
 )
 
+var in = bufio.NewReader(os.Stdin)
+
 func main() {
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
+loop:
+	for {
+		fmt.Println("Please select an option")
+		fmt.Println("1) Print menu")
+		fmt.Println("2) Add item")
+		fmt.Println("q) quit")
+		choice, _ := in.ReadString('\n')
+		//choice = strings.TrimSpace(choice)
 
-	menu := []menuItem{
-		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-	}
-
-	fmt.Println("Please select an option")
-	fmt.Println("1) Print menu")
-	fmt.Println("2) Add item")
-	fmt.Println("3) quit")
-	in := bufio.NewReader(os.Stdin)
-	choice, _ := in.ReadString('\n')
-	//choice = strings.TrimSpace(choice)
-
-	switch strings.TrimSpace(choice) {
-	case "1":
-
-		for _, item := range menu {
-			fmt.Println(item.name)
-			fmt.Println(strings.Repeat("-", 10))
-			for size, price := range item.prices {
-				fmt.Printf("\t%10s%10.2f\n", size, price)
-			}
+		switch strings.TrimSpace(choice) {
+		case "1":
+			menu.Print()
+		case "2":
+			menu.Add()
+		case "q":
+			break loop
+		default:
+			fmt.Println("Unkown option")
 		}
-	case "2":
-		fmt.Println("Please enter the name of the new item")
 	}
 }
